b84fd8723f8406947fca66b0eef3d8840d8509c4
Ethan Pellegrini <epellegr@umich.edu>
2023-05-03 15:25:32 -0400

    Add bounding box conversion to face selection logic

--- reproduce/test.py
+++ reproduce/test.py
@@ -531,7 +531,8 @@ def predict_from_video(opt):
                 
                 if opt.use_facerec and last_known_valid_bbox: #Only use facerec if its ready
                     if opt.facerec == "bbox" and len(fr.known_faces) == 0: #If no known faces, generate a reference image
-                        
+                        logging.info("Boundind Box : {}".format(last_known_valid_bbox))
+                        logging.info("Converted Bbox : {}".format(fr.convert_bounding_boxes([last_known_valid_bbox])))
                         fr.generate_ref_image(fr.convert_bounding_boxes([last_known_valid_bbox]), frame)
                     selected_bbox = fr.select_face(bboxes, frame)
                 else:
